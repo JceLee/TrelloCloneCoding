83780e763b58dcbe387d662abe22a4f46abf4f8c
JceLee <lkm4351@gmail.com>
2021-03-18 23:10:53 +0900

    add elements

--- src/Trello.js
+++ src/Trello.js
@@ -1,5 +1,11 @@
+import MainBoard from "./components/MainBoard";
+import Toolbar from "./components/Toolbar";
+
 export default class Trello {
-  constructor() {}
+  constructor($target) {
+    const mainBoard = new MainBoard($target);
+    const toolbar = new Toolbar($target);
+  }
 
   render() {}
 }
